ac3eab33b315ec5d2ec603b569a4e8af8c986c09
karthikshekhar <karthik@broadinstitute.org>
2016-06-04 11:47:55 -0400

    changed to loop across ids

--- 10X/call_cellranger.sh
+++ 10X/call_cellranger.sh
@@ -3,18 +3,28 @@
 # Wrapper script for 10X pipeline
 
 # To Change 
-run_id=P17_Retina
+run_ids=("P17_Retina1" "P17_Retina2")
+bcs=("SI-3A-A2" "SI-3A-C2")
 bcl_path=/ahg/regev_nextseq/Data/160422_NB501164_0150_AH3MVGBGXY
-trans_path=/ahg/regevdata/projects/10X_runs/REFERENCES/refdata-cellranger-1.0.0/mm10
-bcs=<bc1>,<bc2>,<bc3>,...
+trans_path=/seq/regev_genome_portal/SOFTWARE/10X/refdata-cellranger-1.1.0/mm10
+cellranger_path=/seq/regev_genome_portal/SOFTWARE/10X/cellranger-1.1.0/
 
 # Automatically defined
+n=`expr ${#run_ids[@]} - 1`
 fastq_path=`basename $bcl_path | cut -f 4 -d'_'`
 fastq_path=./${fastq_path/A/}/outs/fastq_path
 
-sed "s|path_to_bcl|${bcl_path}|g;s|my_id|${run_id}|g;s|path_to_fastq|${fastq_path}|g" < /home/unix/karthik/repo/dropseq_scripts/10X/run_10X.sh > run_10X_${run_id}.sh
-sed -i "s|comma_sep_barcodes|${bcs}|g;s|path_to_trans|${trans_path}|g" run_10X_${run_id}.sh
-sed -i "s|error.err|${run_id}.10X.err|g;s|out.log|${run_id}.10X.out|g" run_10X_${run_id}.sh
+# cell ranger run
 
-use uger
-qsub run_10X_${run_id}.sh >> dispatch.txt 2>&1
+for ((i==0; i<=$n; i++)
+do
+  run_id=${run_ids[$i]}
+  barcode=${bcs[$i]}
+  sed "s|path_to_bcl|${bcl_path}|g;s|my_id|${run_id}|g;s|path_to_fastq|${fastq_path}|g" < /home/unix/karthik/repo/dropseq_scripts/10X/run_10X.sh > run_10X_${run_id}.sh
+  sed -i "s|path_to_cellranger|${cellranger_path}|g" run_10X_${run_id}.sh
+  sed -i "s|barcode|${barcode}|g;s|path_to_trans|${trans_path}|g" run_10X_${run_id}.sh
+  sed -i "s|error.err|${run_id}.10X.err|g;s|out.log|${run_id}.10X.out|g" run_10X_${run_id}.sh
+done
+
+#use uger
+#qsub run_10X_${run_id}.sh >> dispatch.txt 2>&1
