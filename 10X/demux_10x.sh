516c85858a2aa77d09ac87af95b2358e1bb40148
karthikshekhar <karthik@broadinstitute.org>
2016-06-04 11:54:08 -0400

    Update demux_10x.sh

--- 10X/demux_10x.sh
+++ 10X/demux_10x.sh
@@ -8,9 +8,8 @@
 
 source /broad/software/scripts/useuse
 reuse -q .bcl2fastq2-2.17.1.14
-export PATH=/seq/regev_genome_portal/SOFTWARE/10X/cellranger-1.1.0:$PATH
-
+cellranger_path=path_to_cellranger
 bclPath=path_to_bcl
 
 # outputs to current directory
-cellranger demux --run=${bclPath}
+${cellranger_path}/cellranger demux --run=${bclPath}
