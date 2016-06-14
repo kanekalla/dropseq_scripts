d6c1ce21eb2ecca48de3014a7962394325d01807
karthikshekhar <karthik@broadinstitute.org>
2016-06-14 09:40:55 -0400

    Create install_gtf_annot.sh

---
+++ build_rnaseq_ref/install_gtf_annot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Author : Karthik Shekhar, 06/14/2016
+# Installing transcriptomic index/reference for RNA-seq data
+# usage : ./install_gtf_annot.sh
+
+# required arguments
+genome_fasta=/path/to/genome.fasta
+gtf=/path/to/gtf
+annot_name=name
+rRNA_fasta=/path/to/rRNA.fasta
+
+# optional arguments
+iso_map=/path/to/genetoisoform_map
+igv_genome
